26860345b8b1a408fde887d2c1c5d8679dee5319
bsucs <122378640+bsucs@users.noreply.github.com>
2026-03-11 11:21:42 -0400

    added final

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,28 +1,54 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 import { store } from '../../store.js'
 import Card from 'primevue/card'
+
+const show = ref(false)
+const flipped = ref(new Set())
+
+function toggleFlip(key) {
+    const next = new Set(flipped.value)
+    if (next.has(key)) {
+        next.delete(key)
+    } else {
+        next.add(key)
+    }
+    flipped.value = next
+}
+
+onMounted(() => {
+    store.fetchData()
+})
 </script>
 
 <template>
-    
-    <h1 class="overview">{{ store.overview }}</h1>
-    
-    <div class="card-grid">
-        
-      <div class="flip-box" v-for="book in store.books" :key="book.order">
-        <div class="flip-box-inner">
-          <div class="flip-box-front">
-            <h2>{{ book.title }} {{ book.order }}</h2>
-          </div>
-          <div class="flip-box-back">
-            <h2>{{ book.author }}</h2>
-          </div>
+    <button @click="show = !show">Toggle</button>
+<Transition>
+    <div v-if="show" class="card-grid">
+        <div class="flip-box" v-for="book in store.books" :key="book.order" @click="toggleFlip(book.order)">
+            <div class="flip-box-inner" :class="{ flipped: flipped.has(book.order) }">
+                <div class="flip-box-front">
+                    <Card style="width: 16rem; height: 100%;">
+                        <template #title>{{ book.title }}</template>
+                        <template #subtitle>{{ book.series }} #{{ book.order }}</template>
+                        <template #content>
+                            <p class="m-0">{{ book.author }} &mdash; {{ book.year }}</p>
+                        </template>
+                    </Card>
+                </div>
+                <div class="flip-box-back">
+                    <Card style="width: 16rem; height: 100%;">
+                        <template #content>
+                            <p>{{ book.description }}</p>
+                        </template>
+                    </Card>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-
-
+    </Transition>
 </template>
+
 <style scoped>
 .card-grid {
   display: flex;
@@ -31,43 +57,32 @@ import Card from 'primevue/card'
 }
 
 .flip-box {
-  background-color: transparent;
-  width: 300px;
+  width: 16rem;
   height: 200px;
-  border: 1px solid #f1f1f1;
-  
+  perspective: 1000px;
 }
 
-/* This container is needed to position the front and back side */
 .flip-box-inner {
   position: relative;
   width: 100%;
   height: 100%;
-  text-align: center;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
 }
 
-/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-box-inner.flipped {
+  transform: rotateY(180deg);
+}
 
-
-/* Position the front and back side */
 .flip-box-front, .flip-box-back {
   position: absolute;
   width: 100%;
   height: 100%;
-  -webkit-backface-visibility: hidden; /* Safari */
+  -webkit-backface-visibility: hidden;
   backface-visibility: hidden;
 }
 
-/* Style the front side */
-.flip-box-front {
-  background-color: #bbb;
-  color: black;
-}
-
-/* Style the back side */
 .flip-box-back {
-  background-color: dodgerblue;
-  color: white;
   transform: rotateY(180deg);
 }
 </style>
